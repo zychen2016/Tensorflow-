9184f4938ae09ffe43e725cf66f2cee6888236ee
zychen2016 <zeyuchen2016@gmail.com>
2018-11-04 21:29:46 +0800

    Update regularizer.py

--- Code/regularizer.py
+++ Code/regularizer.py
@@ -69,3 +69,27 @@ with tf.Session() as sess:
 plt.scatter(X[:,0],X[:,1],c=np.squeeze(Y_c))
 plt.contour(xx,yy,probs,levels=[0.5])
 plt.show()
+#定义反向传播方法：包含正则化
+train_step=tf.train.AdadeltaOptimizer(0.0001).minimize(loss_total)
+with tf.Session() as sess:
+    init_op=tf.global_variables_initializer()
+    sess.run(init_op)
+    STEPS=40000
+    for i in range(STEPS):
+        start=(i*BATCH_SIZE)%300
+        end=start+BATCH_SIZE
+        sess.run(train_step,feed_dict={x:X[start:end],y_:Y_[start:end]})
+        if i%2000==0:
+            loss_v=sess.run(loss_total,feed_dict={x:X,y_:Y_})
+            print("After %d steps, loss is: %f"%(i,loss_v))
+    xx,yy=np.mgrid[-3:3:0.01,-3:3:0.01]
+    grid=np.c_[xx.ravel(),yy.ravel()]
+    probs=sess.run(y,feed_dict={x:grid})
+    probs=probs.reshape(xx.shape)
+    print("w1:\n",sess.run(w1))
+    print("b1:\n",sess.run(b1))
+    print("w2:\n",sess.run(w2))
+plt.scatter(X[:,0],X[:,1],c=np.squeeze(Y_c))
+plt.contour(xx,yy,probs,levels=[0.5])
+plt.show()
+
